e1404e466890ab1c39c2a776ba9700352776fba2
Maria <ev.maria.pro@gmail.com>
2024-08-21 18:45:00 -0400

    Client side: revert to React

---
+++ Client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <h1>Personal Budget Manager</h1>
+      <h2>Envelopes</h2>
+      <p>Transactions:</p>
+    </div>
+  );
+}
+
+export default App;
