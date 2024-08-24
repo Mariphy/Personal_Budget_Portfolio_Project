0d9d24729e76ddbded02be445336d8a7857411ae
Maria <ev.maria.pro@gmail.com>
2024-08-24 17:19:56 -0400

    create transactions component

---
+++ Client/src/components/transactions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Transactions = () => {
+  return (
+    <div>
+      <p>This is the transactions content of the app.</p>
+    </div>
+  );
+};
+
+export default Transactions;
