702cb78c491cef817915a6561b8a20770b821be3
Maria <ev.maria.pro@gmail.com>
2024-08-24 18:22:42 -0400

    /components/envelopes.js display all envelopes, fetch from an api endpoint

--- Client/src/components/envelopes.js
+++ Client/src/components/envelopes.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 const Envelopes = () => {
   const [envelopes, setEnvelopes] = useState([]);  
   useEffect(() => {
-    fetch(
-        //`${process.env.REACT_APP_API_URL}
-        `http://localhost:4001/api/envelopes`, {
+    fetch(`${process.env.REACT_APP_API_URL}/api/envelopes`, {
         method: 'GET'
     })
       .then(response => response.json())
@@ -20,18 +17,20 @@ const Envelopes = () => {
   }, []);
   return (
     <div>
-     <h2>Envelopes</h2>
-      {envelopes.length > 0 ? (
-        envelopes.map(envelope => (
-          <div key={envelope.id}>
-            <h3>{envelope.name}</h3>
-            <p>{envelope.description}</p>
-          </div>
-        ))
-      ) : (
-        <p>No envelopes found.</p>
-      )}
+        <h2>Envelopes</h2>
+        {envelopes.length > 0 ? (
+          envelopes.map(envelope => (
+            <div key={envelope.id}>
+                <h3>{envelope.name}</h3>
+                <p>{envelope.amount}</p>
+            </div>
+          ))  
+        ) : (
+            <p>No envelopes found.</p>
+        )}
+     
     </div>
+    
   );
 };
 
