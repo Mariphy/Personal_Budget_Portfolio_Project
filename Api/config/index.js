4c8a04470a2a0c3680d26ebbe250712ef625efc2
Maria <ev.maria.pro@gmail.com>
2025-04-18 11:07:53 -0400

    delete debugging logs

--- Api/config/index.js
+++ Api/config/index.js
@@ -7,9 +7,6 @@ const pool = new Pool({
 
 const query = (text, params) => {
     return new Promise((resolve, reject) => {
-      pool.query('SELECT NOW()')
-        .then(res => console.log('Connected! Time:', res.rows[0].now))
-        .catch(err => console.error('Connection error:', err.message));
       pool.query(text, params, (error, results) => {
         if (error) {
           return reject(error);
